4729e30cb4da891ad139adfe4414f6fd299b72ef
Andhika Eka Santosa <123168811+andhikaeka3333333@users.noreply.github.com>
2025-01-02 13:59:11 +0700

    Feat:[AE]create drinks API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DrinkController;
 use App\Http\Controllers\FoodController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,4 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
 Route::apiResource('foods', FoodController::class);
+Route::apiResource('drinks', DrinkController::class);
